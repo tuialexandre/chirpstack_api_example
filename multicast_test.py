1e9903565f4ec7c85e5643eb28a1bff7c1d4a2f5
tuialexandre <tuialexandre@gmail.com>
2024-04-23 15:21:37 -0300

    Added single and multicast chirpstack api tests

---
+++ multicast_test.py
@@ -0,0 +1,35 @@
+import grpc
+from chirpstack_api import api
+
+# Configuration.
+
+# This must point to the API interface.
+server = "localhost:8080"
+
+# The MultiCast group for which you want to enqueue the downlink.
+multicast_group_id = "..."
+
+# The API token (retrieved using the web-interface).
+api_token = "..."
+
+if __name__ == "__main__":
+    # Connect without using TLS.
+    channel = grpc.insecure_channel(server)
+
+    # Device-queue API client.
+    client = api.MulticastGroupServiceStub(channel)
+
+    # Define the API key meta-data.
+    auth_token = [("authorization", "Bearer %s" % api_token)]
+
+    # Construct request.
+    req = api.EnqueueMulticastGroupQueueItemRequest()
+    req.queue_item.confirmed = False
+    req.queue_item.data = b"multicast test"
+    req.queue_item.dev_eui = multicast_group_id
+    req.queue_item.f_port = 10
+
+    resp = client.Enqueue(req, metadata=auth_token)
+
+    # Print the downlink id
+    print(resp.id)
